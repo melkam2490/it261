f34cf9e3f359d918a8d73223b7041026bfbefc43
Melkam Worku <melkam2490@gmail.com>
2024-10-02 10:45:34 -0700

    update some of my website and portal pages

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <h1>
             <a href="index.php">Melkam's Portal Page</a>
         </h1>
-        <h2>The navigation below represents our Big Assignment</h2>
+    
     </header>
 
     <div id="wrapper">
@@ -29,7 +29,8 @@
         <main>
             <h2>About Melkam</h2>
             <img class="right" src="images/melka.jpg" alt="Photo of Melkam">
-            <p>A paragraph about myself, i.e., goals, background, hobbies, etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <p>My goal is to become a skilled full-stack developer, mastering both front-end and back-end technologies to build meaningful, user-friendly applications. I’m passionate about solving problems through code and continuously expanding my knowledge in web development. Outside of technology, I love nature and enjoy spending time outdoors, whether it's hiking or simply relaxing by the water. Nature brings me a sense of peace and balance, fueling my creativity and motivation to achieve my goals..</p>
+ 
         </main>
 
         <aside>
@@ -41,12 +42,65 @@
                 <li><a href="weeks/week2/currency.php">currency</a></li>
                 <li><a href="weeks/week2/currency-logic.php">currency-logic</a></li>
                 
-                <li><a href="something.php">Something</a></li> <!-- Corrected link text -->
-                <li><a href="something-else.php">Something Else</a></li> <!-- Suggested a more descriptive link -->
+            
+            </ol>
+            <br>
+            <h3>Week3</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">if.php</a></li>
+                <li><a href="weeks/week2/var2.php">foreach.php</a></li>
+                <li><a href="weeks/week2/currency.php">forloop.php</a></li>
+                <li><a href="weeks/week2/currency-logic.php">date.php</a></li>
+                <li><a href="something.php">Swich.php</a></li> <!-- Corrected link text -->
+                
+            </ol>
+            <br>
+
+            <h3>Week4</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">form1.php</a></li>
+                <li><a href="weeks/week2/var2.php">form2.php</a></li>
+                <li><a href="weeks/week2/currency.php">form3.php</a></li>
+                <li><a href="weeks/week2/currency-logic.php">form-artimatic.php</a></li>
+  
+                
+            </ol>
+            <br>
+            <h3>Week5</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">currency1.php</a></li>
+                <li><a href="weeks/week2/var2.php">currency2.php</a></li>
+                <li><a href="weeks/week2/currency.php">currency3.php</a></li>
+                <li><a href="weeks/week2/currency-logic.php">currency4.php</a></li>
+                <li><a href="something.php">Swich.php</a></li> <!-- Corrected link text -->
+                
+            </ol>
+            <br>
+            <h3>Week6</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">form1.php</a></li>
+                <li><a href="weeks/week2/var2.php">function.php</a></li>
+                <li><a href="weeks/week2/currency.php">impold.php</a></li>
+                
+            </ol>
+            <h3>Week7</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">picture.php</a></li>
+                <li><a href="weeks/week2/var2.php">random.php.php</a></li>
+                <li><a href="weeks/week2/currency.php">impold.php</a></li>
+                
+            </ol>
+            <h3>Week8</h3>
+            <ol>
+                <li><a href="weeks/week2/var.php">people.php</a></li>
+                
+                <li><a href="weeks/week2/var.php">people.php</a></li>
             </ol>
         </aside>
+        <img class="me" src="images/22.jpg" alt=" Melkam">
     </div>
     <!-- end wrapper -->
+   
 
     <footer>
         <ul>
